e0154337fc5bb9d14e5a2f73a4927c5e8112d018
zichy <zichy@zichy.de>
2022-12-15 20:47:36 +0100

    Update viewport meta tag

--- index.php
+++ index.php
@@ -296,7 +296,7 @@ EOD
 <head>
 
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<title>{{SITENAME}}</title>
 
