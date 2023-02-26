f102f7e92a6e64d7d99537f23c059abf862a43b5
zichy <zichy@zichy.de>
2023-02-26 18:31:18 +0100

    Add meta description tag

--- index.php
+++ index.php
@@ -443,6 +443,7 @@ EOD
 
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="{{SITEDESC}}">
 
 	<title>{{SITENAME}}</title>
 
