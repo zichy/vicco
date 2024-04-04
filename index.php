3adb2f2af09d4b11bb76b58f5fd550d5f28e8586
zichy <zichy@zichy.de>
2024-04-04 23:22:50 +0200

    Use classes instead of constants, refactor footer/nav template

--- index.php
+++ index.php
@@ -5,75 +5,70 @@
 	SPDX-FileCopyrightText: Copyright (c) 2015–2023 lawl(†), zichy
 */
 
-// Site name
-const SITENAME = 'vicco';
+class Config {
+	static $username = 'admin';
+	static $passphrase = 'CHANGEME';
+	static $name = 'vicco';
+	static $description = 'Yet another microblog'; // optional
+	static $language = 'en'; // (ISO 639-1)
+	static $dateFormat = 'd M Y, H:i';
+	static $postsPerPage = 10;
+	static $showLogin = true;
+}
 
-// Description (optional)
-const SITEDESC = 'Yet another microblog';
+class Lang {
+	static $author = 'Anonymous';
+	static $search = 'Search';
+	static $placeholder = 'Start typing &hellip;';
+	static $postcontent = 'Post content';
+	static $publish = 'Publish';
+	static $save = 'Save';
+	static $logout = 'Logout';
+	static $edit = 'Edit';
+	static $delete = 'Delete';
+	static $deleteWarning = 'Do you really want to delete this post?';
+	static $older = 'Older';
+	static $newer = 'Newer';
+	static $username = 'Username';
+	static $passphrase = 'Passphrase';
+	static $login = 'Login';
+	static $back = 'Go back';
+	static $error = 'Error';
+	static $errorLogin = 'The credentials are incorrect.';
+	static $errorEmpty = 'Your post must not be empty.';
+	static $errorPostExists = 'A post with this ID already exists.';
+	static $errorPostNonexistent = 'The post you wish to edit does not exist.';
+	static $errorNoResults = 'No search results were found.';
+	static $errorHacker = 'Nice try.';
+}
 
-// Username
-const USERNAME = 'admin';
-
-// Passphrase
-const PASSPHRASE = 'CHANGEME';
-
-// Date format
-const DATEFORMAT = 'd M Y H:i:s';
-
-// Posts per page
-const POSTSPERPAGE = 10;
-
-// Language (ISO 639-1)
-const SITELANG = 'en';
-
-// Language strings
-const L_SEARCH = 'Search';
-const L_PLACEHOLDER = 'Start typing &hellip;';
-const L_POSTCONTENT = 'Post content';
-const L_PUBLISH = 'Publish';
-const L_LOGOUT = 'Logout';
-const L_EDIT = 'Edit';
-const L_DELETE = 'Delete';
-const L_OLDER = 'Older';
-const L_NEWER = 'Newer';
-const L_USERNAME = 'Username';
-const L_PASSPHRASE = 'Passphrase';
-const L_LOGIN = 'Login';
-const L_BACK = 'Go back';
-const L_ERROR = 'Error';
-const L_ERROR_LOGIN = 'The credentials are incorrect.';
-const L_ERROR_EMPTY = 'Your post must not be empty.';
-const L_ERROR_POSTEXISTS = 'A post with this ID already exists.';
-const L_ERROR_POSTNONEXISTENT = 'The post you wish to edit does not exist.';
-const L_ERROR_NORESULTS = 'No search results were found.';
-const L_ERROR_HACKER = 'Nice try.';
-
-// System constants (Do not change)
-const DATAPATH = 'vicco/';
-const KEY = 'key';
-const VALUE = 'value';
-const DB = 'db';
-const CSS = 'style.css';
-const JS = 'script.js';
-const D_POSTCONTENT = 'postcontent';
-const D_POSTDATE = 'postdate';
-const D_POSTDATETIME = 'postdatetime';
-const D_POSTID = 'postid';
-const COOKIE = 'cookie';
+class Sys {
+	static $path = 'vicco/';
+	static $key = 'key';
+	static $value = 'value';
+	static $db = 'db';
+	static $css = 'style.css';
+	static $js = 'script.js';
+	static $postContent = 'postcontent';
+	static $postDate = 'postdate';
+	static $postDateTime = 'postdatetime';
+	static $postId = 'postid';
+	static $cookie = 'cookie';
+}
 
 session_start();
 
 // Installation
-if(get_kvp(DB, 'firstuse') === false) {
+if(get_kvp(Sys::$db, 'firstuse') === false) {
 	if(!record_exists('')) {
-		if(!mkdir(DATAPATH)) {
-			die('No write permissions to create the folder "'.DATAPATH.'".');
+		if(!mkdir(Sys::$path)) {
+			die('No write permissions to create the folder "'.Sys::$path.'".');
 		}
 	}
-	create_record(DB);
-	create_index(D_POSTDATE, D_POSTDATE);
+	create_record(Sys::$db);
+	create_index(Sys::$postDate, Sys::$postDate);
 
-	set_file(null, CSS, <<< 'EOD'
+	set_file(null, Sys::$css, <<< 'EOD'
 :root {
 	--f-sans: system-ui, sans-serif;
 	--f-mono: ui-monospace, monospace;
@@ -262,7 +257,7 @@ nav {
 
 EOD
 	);
-	set_file(null, JS, <<< 'EOD'
+	set_file(null, Sys::$js, <<< 'EOD'
 const $textarea = document.getElementById('postcontent');
 if($textarea) {
 	function resizeArea($el) {
@@ -280,8 +275,9 @@ if($textarea) {
 const $adminForms = document.querySelectorAll('.admin');
 if($adminForms) {
 	$adminForms.forEach(($form) => {
+		const warning = $form.dataset.warning;
 		$form.addEventListener('submit', (e) => {
-			if(confirm()) {
+			if(confirm(warning)) {
 				$form.submit();
 			} else {
 				e.preventDefault();
@@ -291,58 +287,58 @@ if($adminForms) {
 }
 
 EOD
-	); set_kvp(DB, 'firstuse', 1);
+	); set_kvp(Sys::$db, 'firstuse', 1);
 }
 
 // Database
 function create_record($r) {
 	$r = sanitize_key($r);
 	if(!record_exists($r)) {
-		mkdir(DATAPATH.$r);
+		mkdir(Sys::$path.$r);
 	}
 	return $r;
 }
 
 function set_file($r, $k, $v) {
-	file_put_contents(DATAPATH.$r.'/'.$k,$v);
+	file_put_contents(Sys::$path.$r.'/'.$k,$v);
 }
 
 function set_kvp($r, $k, $v) {
-	$f = DATAPATH.sanitize_key($r).'/'.sanitize_key($k);
+	$f = Sys::$path.sanitize_key($r).'/'.sanitize_key($k);
 	file_put_contents($f,$v);
 	chmod($f, 0600);
 }
 
 function get_kvp($r, $k) {
-	$p = DATAPATH.sanitize_key($r).'/'.sanitize_key($k);
+	$p = Sys::$path.sanitize_key($r).'/'.sanitize_key($k);
 	return file_exists($p) ? file_get_contents($p) : false;
 }
 
 function delete_kvp($r, $kvp) {
-	unlink(DATAPATH.sanitize_key($r).'/'.sanitize_key($kvp));
+	unlink(Sys::$path.sanitize_key($r).'/'.sanitize_key($kvp));
 }
 
 function record_exists($p) {
 	$p = sanitize_key($p);
-	return file_exists(DATAPATH.$p) && is_dir(DATAPATH.$p);
+	return file_exists(Sys::$path.$p) && is_dir(Sys::$path.$p);
 }
 
 function record_delete($r) {
 	$r = sanitize_key($r);
 	if(record_exists($r)) {
-		$h = opendir(DATAPATH.$r);
+		$h = opendir(Sys::$path.$r);
 		for($i = 0; ($e = readdir($h)) !== false; $i++) {
 			if ($e != '.' && $e != '..' ) {
-				unlink(DATAPATH.$r.'/'.$e);
+				unlink(Sys::$path.$r.'/'.$e);
 			}
 		}
 		closedir($h);
-		rmdir(DATAPATH.$r);
+		rmdir(Sys::$path.$r);
 	}
 }
 
 function get_keys($r) {
-	$s = scandir(DATAPATH.$r);
+	$s = scandir(Sys::$path.$r);
 	return array_values(array_filter($s, function($v) {
 		return $v != '.' && $v != '..';
 	}));
@@ -354,49 +350,73 @@ function sanitize_key($k) {
 
 function create_index($n, $k) {
 	$d = array();
-	$h = opendir(DATAPATH);
+	$h = opendir(Sys::$path);
 	for($i = 0; ($e = readdir($h)) !== false; $i++) {
-		if ($e != '.' && $e != '..' && $e != DB) {
-			$d[$i][KEY] = $e;
-			$d[$i][VALUE] = get_kvp($e, $k);
-			if($d[$i][VALUE] === false) {
+		if ($e != '.' && $e != '..' && $e != Sys::$db) {
+			$d[$i][Sys::$key] = $e;
+			$d[$i][Sys::$value] = get_kvp($e, $k);
+			if($d[$i][Sys::$value] === false) {
 				array_pop($d);
 			}
 		}
 	}
 	closedir($h);
-	set_kvp(DB, 'index_'.$n, serialize($d));
+	set_kvp(Sys::$db, 'index_'.$n, serialize($d));
 }
 
 function get_index($n) {
-	return unserialize(get_kvp(DB, 'index_'.$n));
+	return unserialize(get_kvp(Sys::$db, 'index_'.$n));
 }
 
 // Template function
 function tpl() {
 	$f = func_get_args();
 	$n = sizeof($f) - 1;
-	$t = get_kvp(DB, $f[0]);
+	$t = get_kvp(Sys::$db, $f[0]);
 	for($i = 1; $i < $n; $i += 2) {
 		$t = str_replace('{{'.$f[$i].'}}', $f[$i + 1], $t);
 	}
 	return $t;
 }
 
-// Templates
-function footer() { ?>
+// Footer template
+function footer($results = 0) { ?>
 	</main>
+	<footer class="footer row">
+		<?php 
+			if(!isset($_GET['p']) && !isset($_GET['edit']) && $results >= Config::$postsPerPage) { ?>
+				<nav class="row">
+					<?php if (@$_GET['skip'] > 0): ?>
+						<a href="?skip=<?= (@$_GET['skip'] > 0 ? @$_GET['skip'] - Config::$postsPerPage : 0).'&amp;s='.@urlencode($_GET['s']) ?>" class="button">&larr; <?= Lang::$newer ?></a>
+					<?php endif ?>
+					<?php if (@$_GET['skip'] + Config::$postsPerPage < $results): ?>
+						<a href="?skip=<?= (@$_GET['skip'] + Config::$postsPerPage < $results ? @$_GET['skip'] + Config::$postsPerPage : @(int)$_GET['skip']).'&amp;s='.@urlencode($_GET['s']) ?>" class="button"><?= Lang::$older ?> &rarr;</a>
+					<?php endif ?>
+				</nav>
+			<?php }
+		?>
+
+		<?php if(Config::$showLogin && !isset($_GET['login']) && !loggedin()): ?>
+			<a class="button" href="?login">Login</a>
+		<?php elseif(loggedin()): ?>
+			<form action="/" method="post">
+				<button type="submit" name="logout"><?= Lang::$logout ?></button>
+			</form>
+		<?php endif ?>
+	</footer>
+
 	<?php if (loggedin()): ?>
-		<script src="<?= DATAPATH.JS ?>"></script>
+		<script src="<?= Sys::$path.Sys::$js ?>"></script>
 	<?php endif ?>
 	</body></html>
 <?php }
 
+// Error template
 function error($text) { ?>
 	<section class="box">
-		<h2><?= L_ERROR ?></h2>
+		<h2><?= Lang::$error ?></h2>
 		<p><?= $text ?>
-		<p><a class="button" href="/"><?= L_BACK ?></a>
+		<p><a class="button" href="/"><?= Lang::$back ?></a>
 	</section>
 <?php 
 	footer();
@@ -423,25 +443,28 @@ function parse($t) {
 
 // Feed
 if(isset($_GET['feed'])) {
-	$p = @array_slice(get_index(D_POSTDATE), 0, POSTSPERPAGE);
+	$p = @array_slice(get_index(Sys::$postDate), 0, Config::$postsPerPage);
 	$u = "https://$_SERVER[HTTP_HOST]";
 	$f = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 	header('Content-type: application/atom+xml'); ?>
 <?xml version="1.0" encoding="utf-8"?>
 <feed xmlns="http://www.w3.org/2005/Atom">
-<title><?= SITENAME ?></title>
-<?php if (!empty(SITEDESC)): ?>
-<subtitle><?= SITEDESC ?></subtitle>
+<title><?= Config::$name ?></title>
+<?php if (!empty(Config::$description)): ?>
+<subtitle><?= Config::$description ?></subtitle>
+<author>
+	<name><?= Lang::$author ?></name>
+</author>
 <?php endif ?>
 <link href="<?= $u ?>" />
 <link href="<?= $f ?>" rel="self"/>
 <?php foreach($p as $m): ?>
 <entry>
-	<title><?= date(DATEFORMAT, $m[VALUE]) ?></title>
-	<link href="<?= $u . '?p='.$m[KEY] ?>" />
-	<content type="html"><![CDATA[<?= parse(nl2br(get_kvp($m[KEY], D_POSTCONTENT))) ?>]]></content>
-	<updated><?= date('Y-m-d\TH:i:sP', $m[VALUE]) ?></updated>
-	<id>urn:uuid:<?= $m[KEY] ?></id>
+	<title><?= date(Config::$dateFormat, $m[Sys::$value]) ?></title>
+	<link href="<?= $u . '?p='.$m[Sys::$key] ?>" />
+	<content type="html"><![CDATA[<?= parse(nl2br(get_kvp($m[Sys::$key], Sys::$postContent))) ?>]]></content>
+	<updated><?= date('Y-m-d\TH:i:sP', $m[Sys::$value]) ?></updated>
+	<id>urn:uuid:<?= $m[Sys::$key] ?></id>
 </entry>
 <?php endforeach ?>
 </feed><?php die();
@@ -449,18 +472,18 @@ if(isset($_GET['feed'])) {
 
 // Header
 ?>
-<!DOCTYPE html><html lang="<?= SITELANG ?>"><head>
+<!DOCTYPE html><html lang="<?= Config::$language ?>"><head>
 
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<?php if (!empty(SITEDESC)): ?>
-		<meta name="description" content="<?= SITEDESC ?>">
+	<?php if (!empty(Config::$description)): ?>
+		<meta name="description" content="<?= Config::$description ?>">
 	<?php endif ?>
 
-	<title><?= SITENAME ?></title>
+	<title><?= Config::$name ?></title>
 
-	<link href="/?feed" type="application/atom+xml" title="<?= SITENAME ?> feed" rel="alternate">
-	<link rel="stylesheet" type="text/css" href="<?= DATAPATH.CSS ?>" media="screen">
+	<link href="/?feed" type="application/atom+xml" title="<?= Config::$name ?> feed" rel="alternate">
+	<link rel="stylesheet" type="text/css" href="<?= Sys::$path.Sys::$css ?>" media="screen">
 
 </head>
 
@@ -469,20 +492,20 @@ if(isset($_GET['feed'])) {
 <header>
 	<div>
 		<h1 itemprop="name">
-			<?php if (!empty($_GET)): ?>
-				<a href="/"><?= SITENAME ?></a>
-			<?php else: ?>
-				<?= SITENAME ?>
-			<?php endif ?>
-		</h1>
-		<?php if (!empty(SITEDESC)): ?>
-			<p itemprop="description"><?= SITEDESC ?></p>
+		<?php if (!empty($_GET)): ?>
+			<a href="/"><?= Config::$name ?></a>
+		<?php else: ?>
+			<?= Config::$name ?>
 		<?php endif ?>
+		</h1>
+	<?php if (!empty(Config::$description)): ?>
+		<p itemprop="description"><?= Config::$description ?></p>
+	<?php endif ?>
 	</div>
 
 	<form class="search" action="/" method="get" role="search">
-		<input type="text" name="s" aria-label="<?= L_SEARCH ?>">
-		<button type="submit"><?= L_SEARCH ?></button>
+		<input type="text" name="s" aria-label="<?= Lang::$search ?>">
+		<button type="submit"><?= Lang::$search ?></button>
 	</form>
 </header>
 
@@ -492,17 +515,17 @@ if(isset($_GET['feed'])) {
 // Cookie
 function set_cookie() {
 	$identifier = bin2hex(random_bytes('64'));
-	set_kvp(DB, COOKIE, $identifier);
+	set_kvp(Sys::$db, Sys::$cookie, $identifier);
 	setcookie('vicco', $identifier, time()+(3600*24*30));
 }
 function delete_cookie() {
-	delete_kvp(DB, COOKIE);
+	delete_kvp(Sys::$db, Sys::$cookie);
 	setcookie('vicco', '', time()-(3600*24*30));
 }
 
 // Login
 function loggedin() {
-	if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true && isset($_COOKIE['vicco']) && $_COOKIE['vicco'] === tpl(COOKIE)) {
+	if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true && isset($_COOKIE['vicco']) && $_COOKIE['vicco'] === tpl(Sys::$cookie)) {
 		return true;
 	}
 }
@@ -512,14 +535,14 @@ if(isset($_GET['login'])) {
 	} else { ?>
 		<form class="box login" action="/" method="post">
 			<div>
-				<label for="username"><?= L_USERNAME ?></label>
+				<label for="username"><?= Lang::$username ?></label>
 				<input type="text" id="username" name="username" autocomplete="username">
 			</div>
 			<div>
-				<label for="passphrase"><?= L_PASSPHRASE ?></label>
+				<label for="passphrase"><?= Lang::$passphrase ?></label>
 				<input type="password" id="passphrase" name="passphrase" autocomplete="current-password">
 			</div>
-			<button type="submit" name="login"><?= L_LOGIN ?></button>
+			<button type="submit" name="login"><?= Lang::$login ?></button>
 		</form>
 	<?php
 		footer();
@@ -527,38 +550,38 @@ if(isset($_GET['login'])) {
 	}
 }
 if(isset($_POST['login'])) {
-	if(hash_equals(USERNAME, $_POST['username']) && hash_equals(PASSPHRASE, $_POST['passphrase'])) { 
+	if(hash_equals(Config::$username, $_POST['username']) && hash_equals(Config::$passphrase, $_POST['passphrase'])) { 
 		$_SESSION['loggedin'] = true;
 		set_cookie();
 		rmain();
 	} else {
-		error(L_ERROR_LOGIN);
+		error(Lang::$errorLogin);
 	}
 }
 if(loggedin()) {
 	// Submit posts
 	if(isset($_POST['submit'])) {
 		$r = 0;
-		if(empty($_POST[D_POSTCONTENT])) {
-			error(L_ERROR_EMPTY);
+		if(empty($_POST[Sys::$postContent])) {
+			error(Lang::$errorEmpty);
 		}
-		if(empty($_POST[D_POSTID])) {
+		if(empty($_POST[Sys::$postId])) {
 			$r = create_record(uniqid());
-			set_kvp($r, D_POSTDATE, time());
+			set_kvp($r, Sys::$postDate, time());
 		} else {
-			if(!record_exists($_POST[D_POSTID])) {
-				error(L_ERROR_POSTEXISTS);
+			if(!record_exists($_POST[Sys::$postId])) {
+				error(Lang::$errorPostExists);
 			}
-			$r = $_POST[D_POSTID];
+			$r = $_POST[Sys::$postId];
 		}
-		set_kvp($r, D_POSTCONTENT, $_POST[D_POSTCONTENT]);
-		create_index(D_POSTDATE, D_POSTDATE);
+		set_kvp($r, Sys::$postContent, $_POST[Sys::$postContent]);
+		create_index(Sys::$postDate, Sys::$postDate);
 	}
 
 	// Delete posts
 	if(isset($_POST['delete'])) {
 		record_delete($_POST['postid']);
-		create_index(D_POSTDATE, D_POSTDATE);
+		create_index(Sys::$postDate, Sys::$postDate);
 	} 
 
 	// Edit posts
@@ -569,21 +592,20 @@ if(loggedin()) {
 	}
 
 	if (editing() && !record_exists($_GET['edit'])) {
-		error(L_ERROR_POSTNONEXISTENT);
+		error(Lang::$errorPostNonexistent);
 	} ?>
 
 	<form class="box panel" action="/" method="post">
 		<input type="hidden" name="postid" id="postid" value="<?= (editing() ? $_GET['edit'] : '') ?>">
-		<textarea id="postcontent" name="postcontent" placeholder="<?= L_PLACEHOLDER ?>" aria-label="<?= L_POSTCONTENT ?>" spellcheck="false" rows="1" autofocus><?= (editing() ? get_kvp($_GET['edit'], D_POSTCONTENT) : '') ?></textarea>
+		<textarea id="postcontent" name="postcontent" placeholder="<?= Lang::$placeholder ?>" aria-label="<?= Lang::$postcontent ?>" spellcheck="false" rows="1" autofocus><?= (editing() ? get_kvp($_GET['edit'], Sys::$postContent) : '') ?></textarea>
 
 		<div class="panel-meta row">
-			<button type="submit" id="submit" name="submit"><?= L_PUBLISH ?></button>
-			<button type="submit" name="logout"><?= L_LOGOUT ?></button>
+			<button type="submit" id="submit" name="submit"><?= (editing() ? Lang::$save : Lang::$publish) ?></button>
 		</div>
 	</form>
 
 <?php } elseif(isset($_POST['submit']) || isset($_POST['delete']) || (isset($_GET['edit']))) {
-	error(L_ERROR_HACKER);
+	error(Lang::$hacker);
 }
 
 // Logout
@@ -594,13 +616,13 @@ if(isset($_POST['logout'])) {
 }
 
 // Posts
-$p = get_index(D_POSTDATE);
+$p = get_index(Sys::$postDate);
 
 // Search
 if(!empty($_GET['s'])) {
 	$s = explode(' ',$_GET['s']);
 	foreach($p as $k => $m) {
-		$c = strtolower(parse(nl2br(get_kvp($m[KEY], D_POSTCONTENT))));
+		$c = strtolower(parse(nl2br(get_kvp($m[Sys::$key], Sys::$postContent))));
 		$f = true;
 		for($i = 0; $i < sizeof($s); $i++) {
 			if(strpos($c, strtolower($s[$i])) === false) {
@@ -615,59 +637,47 @@ if(!empty($_GET['s'])) {
 }
 $results = sizeof($p);
 if($results == 0) {
-	error(L_ERROR_NORESULTS);
+	error(Lang::$errorNoResults);
 }
 
 // Sorting
 uasort($p, function($a, $b) {
-	if($a[VALUE] == $b[VALUE]) {
+	if($a[Sys::$value] == $b[Sys::$value]) {
 		return 0;
 	} else {
-		return $b[VALUE] <=> $a[VALUE];
+		return $b[Sys::$value] <=> $a[Sys::$value];
 	}
 });
 
 if(isset($_GET['p']) && record_exists($_GET['p'])) {
-	$p = array(array(VALUE => get_kvp($_GET['p'], D_POSTDATE), KEY => $_GET['p']));
+	$p = array(array(Sys::$value => get_kvp($_GET['p'], Sys::$postDate), Sys::$key => $_GET['p']));
 }
 
-$p = @array_slice($p, $_GET['skip'], POSTSPERPAGE);
+$p = @array_slice($p, $_GET['skip'], Config::$postsPerPage);
 
 if(!isset($_GET['edit'])) {
 	foreach($p as $m): ?>
 		<article class="box post" itemscope itemtype="https://schema.org/BlogPosting">
-			<p class="post-text" itemprop="articleBody"><?= parse(nl2br(get_kvp($m[KEY], D_POSTCONTENT))) ?></p>
+			<p class="post-text" itemprop="articleBody"><?= parse(nl2br(get_kvp($m[Sys::$key], Sys::$postContent))) ?></p>
 			<footer class="post-meta row">
 				<div>
-					<a href="?p=<?= $m[KEY] ?>" itemprop="url">
-						<time datetime="<?= date('Y-m-d H:i:s', $m[VALUE]) ?>" itemprop="datePublished" pubdate><?= date(DATEFORMAT, $m[VALUE]) ?></time>
+					<a class="permalink" href="?p=<?= $m[Sys::$key] ?>" itemprop="url">
+						<time datetime="<?= date('Y-m-d H:i:s', $m[Sys::$value]) ?>" itemprop="datePublished" pubdate><?= date(Config::$dateFormat, $m[Sys::$value]) ?></time>
 					</a>
 				</div>
-				<?php if (loggedin()): ?>
-					<form class="admin row" action="/" method="post">
-					<input type="hidden" name="postid" value="<?= $m[KEY] ?>">
-					<a class="button" href="?edit=<?= $m[KEY] ?>"><?= L_EDIT ?></a>
-					<button type="submit" class="delete" name="delete"><?= L_DELETE ?></button>
+			<?php if (loggedin()): ?>
+				<form class="admin row" action="/" method="post" data-warning="<?= Lang::$deleteWarning ?>">
+					<input type="hidden" name="postid" value="<?= $m[Sys::$key] ?>">
+					<a class="button" href="?edit=<?= $m[Sys::$key] ?>"><?= Lang::$edit ?></a>
+					<button type="submit" class="delete" name="delete"><?= Lang::$delete ?></button>
 				</form>
-				<?php endif ?>
+			<?php endif ?>
 			</footer>
 		</article>
 	<?php endforeach;
 }
 
-// Navigation
-if(!isset($_GET['p']) && !isset($_GET['edit']) && $results >= POSTSPERPAGE) { ?>
-	<nav class="row">
-		<?php if (@$_GET['skip'] > 0): ?>
-			<a href="?skip=<?= (@$_GET['skip'] > 0 ? @$_GET['skip'] - POSTSPERPAGE : 0).'&amp;s='.@urlencode($_GET['s']) ?>" class="button"><?= L_NEWER ?></a>
-		<?php endif ?>
-		<?php if (@$_GET['skip'] + POSTSPERPAGE < $results): ?>
-			<a href="?skip=<?= (@$_GET['skip'] + POSTSPERPAGE < $results ? @$_GET['skip'] + POSTSPERPAGE : @(int)$_GET['skip']).'&amp;s='.@urlencode($_GET['s']) ?>" class="button"><?= L_OLDER ?></a>
-		<?php endif ?>
-	</nav>
-<?php }
-
 // Footer
-footer();
+footer($results);
 
 ?>
